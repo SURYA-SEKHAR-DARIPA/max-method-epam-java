0a22810024ec9f292f47a727f52f764b1db0709a
Surya <suryasekhardaripa60461@gmail.com>
2023-01-27 11:19:32 +0530

    Done

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -2,6 +2,14 @@ package com.epam.rd.autotasks.max;
 
 public class MaxMethod {
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+
+        int maximum=values[0];
+        for(int i=1;i<values.length;i++){
+            if(values[i]>maximum){
+                maximum=values[i];
+            }
+        }
+        return maximum;
+        //throw new UnsupportedOperationException();
     }
 }
